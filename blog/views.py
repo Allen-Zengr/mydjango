d59df912ed191440338825c0d409a6a04a8e5a46
PythonAllen <531116448@qq.com>
2017-06-22 22:08:30 +0800

    完整的示例
    完整的示例

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from blog.models import BlogsPost
+from django.shortcuts import render_to_response
+
+
+# Create your views here.
+
+def index(requset):
+     blog_list = BlogsPost.objects.all()
+     return render_to_response('index.html', {'blog_list': blog_list})
